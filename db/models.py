38896187abeb8bb4d2fd8fc51f898035b4e8fb2f
Anatolii Hrechaniuk <genezisium@gmail.com>
2025-12-26 13:16:32 +0200

    Solution3

--- db/models.py
+++ db/models.py
@@ -13,7 +13,7 @@ class Actor(models.Model):
     last_name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+        return "{0} {1}".format(self.first_name, self.last_name)
 
 
 class Movie(models.Model):
@@ -47,4 +47,4 @@ class MovieSession(models.Model):
                               related_name="movie_sessions")
 
     def __str__(self) -> str:
-        return f"{self.movie.title} {self.show_time}"
+        return "{0} {1}".format(self.movie.title, self.show_time)
